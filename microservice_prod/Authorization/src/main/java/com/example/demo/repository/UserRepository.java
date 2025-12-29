6f76cc9ab29902d5ecab85a433505602a3631405
Yokubjon <yokubjon_nurullaev@mail.ru>
2025-12-30 00:24:29 +0900

    Update UserRepository.java

--- microservice_prod/Authorization/src/main/java/com/example/demo/repository/UserRepository.java
+++ microservice_prod/Authorization/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,15 +1,13 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
-
+import com.example.demo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.example.demo.model.User;
+import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-   
+
     Optional<User> findByEmail(String email);
 
-    // searching by username
     Optional<User> findByUsername(String username);
 }
