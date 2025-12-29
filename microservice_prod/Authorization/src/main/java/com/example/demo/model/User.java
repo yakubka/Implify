77d65a1e05b8392cfa9567a96435248f412aadfb
Yokubjon <yokubjon_nurullaev@mail.ru>
2025-12-30 00:25:09 +0900

    Update User.java

--- microservice_prod/Authorization/src/main/java/com/example/demo/model/User.java
+++ microservice_prod/Authorization/src/main/java/com/example/demo/model/User.java
@@ -45,7 +45,6 @@ public class User {
     private String twoFactorSecret;
 
     private LocalDateTime lastLoginAt;
-
     private LocalDateTime deletedAt;
 
     @CreatedDate
@@ -58,5 +57,26 @@ public class User {
         return deletedAt != null;
     }
 
-    
+    // ===== Getters & Setters =====
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public Set<Role> getRoles() { return roles; }
+    public void setRoles(Set<Role> roles) { this.roles = roles; }
+
+    public boolean isEnabled() { return enabled; }
+    public void setEnabled(boolean enabled) { this.enabled = enabled; }
+
+    public LocalDateTime getLastLoginAt() { return lastLoginAt; }
+    public void setLastLoginAt(LocalDateTime lastLoginAt) { this.lastLoginAt = lastLoginAt; }
 }
