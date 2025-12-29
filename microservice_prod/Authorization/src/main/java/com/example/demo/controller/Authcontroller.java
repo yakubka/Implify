1a2f1fc26d6d3ea9689a09437876d64a0b5f841f
Yokubjon <yokubjon_nurullaev@mail.ru>
2025-12-30 00:22:01 +0900

    Update Authcontroller.java

--- microservice_prod/Authorization/src/main/java/com/example/demo/controller/Authcontroller.java
+++ microservice_prod/Authorization/src/main/java/com/example/demo/controller/Authcontroller.java
@@ -1,43 +1,30 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.model.User;
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.Authservice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class Authcontroller {
 
-    @Autowired
-    private Authservice authService;
+    private final Authservice authService;
 
-    // Regestartion 
+    public Authcontroller(Authservice authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody User user) {
-        boolean ok = authService.register(user);
-        if (!ok) {
-            return ResponseEntity.badRequest().body("Пользователь с таким логином уже есть!");
-        }
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        authService.register(request);
         return ResponseEntity.ok("Регистрация успешна!");
     }
 
-    // Login
-    
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestParam String username,
-                                   @RequestParam String password) {
-        boolean ok = authService.login(username, password);
-        if (!ok) {
-            return ResponseEntity.status(401).body("Неверный логин или пароль!");
-        }
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        authService.login(request);
         return ResponseEntity.ok("Вы успешно вошли!");
     }
 }
